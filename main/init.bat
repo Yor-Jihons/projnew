89b550aec621b05e7f99bcce473f47176ec3f815
Yor-Jihons <ryo.malon814@gmail.com>
2025-12-17 15:40:42 +0900

    feat: Install the System.CommandLine.

--- main/init.bat
+++ main/init.bat
@@ -1,3 +1,4 @@
 @echo off
 dotnet add package System.Memory --version 4.6.3 -s .\pkg
 dotnet add package spectre.console --version 0.50.0 -s .\pkg
+dotnet add package System.CommandLine --version 2.0.1
