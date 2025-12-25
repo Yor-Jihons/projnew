a3342333b29cba7a1ca7624b110792f674a5029e
Yor-Jihons <ryo.malon814@gmail.com>
2025-12-25 15:55:35 +0900

    feat: Add the DefinitionFileGenerationProcessor.

---
+++ main/src/Processors/DefinitionFileGenerationProcessor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+using System.Threading;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Linq;
+using ProjNew.Defintions;
+
+namespace ProjNew.Processors
+{
+    public class DefinitionFileGenerationProcessor : IProcessor
+    {
+        public void Run( TemplateConfig templateConfig )
+        {
+            
+        }
+    }
+}
