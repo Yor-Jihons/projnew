07c7e03a0e28baa20126d5dae492d74b5ef16257
Yor-Jihons <ryo.malon814@gmail.com>
2025-12-26 15:51:23 +0900

    feat: JSONファイルに合わせるためにSystem.Text.Json.Serialization.JsonPropertyName属性を付与

--- main/src/Definitions/FileReplacement.cs
+++ main/src/Definitions/FileReplacement.cs
@@ -7,10 +7,13 @@ namespace ProjNew.Defintions
 {
     public class FileReplacement
     {
+        [System.Text.Json.Serialization.JsonPropertyName("fileName")]
         public string FileName{ get; set; } = "";
 
+        [System.Text.Json.Serialization.JsonPropertyName("placeholder")]
         public string Placeholder{ get; set; } = "";
 
+        [System.Text.Json.Serialization.JsonPropertyName("replacementValue")]
         public string ReplacementValue{ get; set; } = "";
     }
 }
--- main/src/Definitions/TemplateDefinition.cs
+++ main/src/Definitions/TemplateDefinition.cs
@@ -7,18 +7,26 @@ namespace ProjNew.Defintions
 {
     public class TemplateDefinition
     {
+        [System.Text.Json.Serialization.JsonPropertyName("id")]
         public string Id{ get; set; } = "";
 
+        [System.Text.Json.Serialization.JsonPropertyName("description")]
         public string Description{ get; set; } = "";
 
+        [System.Text.Json.Serialization.JsonPropertyName("sourceType")]
         public string SourceType{ get; set; } = "git";
 
+        [System.Text.Json.Serialization.JsonPropertyName("sourceUrl")]
         public string SourceUrl{ get; set; } = "https://example.com/projnew-sample-template.git";
 
+
+        [System.Text.Json.Serialization.JsonPropertyName("defaultBranch")]
         public string DefaultBranch{ get; set; } = "main";
 
+        [System.Text.Json.Serialization.JsonPropertyName("fileReplacements")]
         public List<FileReplacement> FileReplacements{ get; set; } = [];
 
+        [System.Text.Json.Serialization.JsonPropertyName("postCloneActions")]
         public List<string> PostCloneActions{ get; set; } = [];
     }
 }
