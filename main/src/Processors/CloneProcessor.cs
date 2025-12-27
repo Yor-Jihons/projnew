143bab476947742b4a9aee2ccde4fdb357b3e184
Yor-Jihons <ryo.malon814@gmail.com>
2025-12-27 18:14:12 +0900

    feat: Getting the arguments.

--- main/src/Processors/CloneProcessor.cs
+++ main/src/Processors/CloneProcessor.cs
@@ -18,6 +18,14 @@ namespace ProjNew.Processors
             // 4. gitを外部プロセス起動で呼び出す
             // 5. 初回起動時はPostCloneActionsの直前に「自己責任で使いましょう」とメッセージを出す
             // 6. Yesを選択したらPostCloneActionsの処理を外部プロセス起動で呼び出す
+
+            var templates = templateConfig.Templates.Where( obj => string.Equals( obj.Id, cmdLine.Template ) ).ToList();
+            if (templates.Count == 0)
+            {
+                throw new Exception( "Not found the template." ); // TODO: Implement the exception.
+            }
+
+            Console.WriteLine( templates[0] );
         }
     }
 }
