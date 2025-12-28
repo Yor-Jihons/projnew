2ea8c1daffdddffd78defd92318696e20b64f57f
Yor-Jihons <ryo.malon814@gmail.com>
2025-12-28 17:12:26 +0900

    feat: テストクラスの追加

---
+++ main.xTests/CmdLineTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using ProjNew.Processors;
+using System.Diagnostics.Contracts;
+using System;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+
+
+namespace main.xTests;
+
+public class CmdLineTest
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
